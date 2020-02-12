c335cdce985f3fd2da98c2b2edf125b4e1be502c
Vence Lam <vence722@users.noreply.github.com>
2020-02-12 12:27:42 +0800

    add functions Str2XXXWithDefaultValue

--- convert.go
+++ convert.go
@@ -76,142 +76,274 @@ func internalStr2Int(val string, bitSize int, noPanic bool) (int64, error) {
 // Simply return the conversion result.
 // For conversions that may panic, the code won't come here.
 // For conversions that won't panic, res will be zero value and err will come here as well.
-func wrapResult(res int64, err error) int64 {
+func wrapInt64Result(res int64, err error) int64 {
 	return res
 }
 
 // Convert string to int64, panic when the string is not of a valid int64 format.
 func Str2Int64(val string) int64 {
-	return int64(wrapResult(internalStr2Int(val, 64, false)))
+	return int64(wrapInt64Result(internalStr2Int(val, 64, false)))
 }
 
 // Convert string to int64, return 0 if the conversion fails
 func Str2Int64NoPanic(val string) int64 {
-	return int64(wrapResult(internalStr2Int(val, 64, true)))
+	return int64(wrapInt64Result(internalStr2Int(val, 64, true)))
+}
+
+// Convert string to int64, return the default value you set when the conversion fails
+func Str2Int64WithDefaultValue(val string, defaultVal int64) int64 {
+	if res, err := internalStr2Int(val, 64, false); err != nil {
+		return defaultVal
+	} else {
+		return int64(res)
+	}
 }
 
 // Convert string to int32, panic when the string is not of a valid int32 format.
 func Str2Int32(val string) int32 {
-	return int32(wrapResult(internalStr2Int(val, 32, false)))
+	return int32(wrapInt64Result(internalStr2Int(val, 32, false)))
 }
 
 // Convert string to int32, return 0 if the conversion fails
 func Str2Int32NoPanic(val string) int32 {
-	return int32(wrapResult(internalStr2Int(val, 32, true)))
+	return int32(wrapInt64Result(internalStr2Int(val, 32, true)))
+}
+
+// Convert string to int32, return the default value you set when the conversion fails
+func Str2Int32WithDefaultValue(val string, defaultVal int32) int32 {
+	if res, err := internalStr2Int(val, 32, false); err != nil {
+		return defaultVal
+	} else {
+		return int32(res)
+	}
 }
 
 // Convert string to int16, panic when the string is not of a valid int16 format.
 func Str2Int16(val string) int16 {
-	return int16(wrapResult(internalStr2Int(val, 16, false)))
+	return int16(wrapInt64Result(internalStr2Int(val, 16, false)))
 }
 
 // Convert string to int16, return 0 if the conversion fails
 func Str2Int16NoPanic(val string) int16 {
-	return int16(wrapResult(internalStr2Int(val, 16, true)))
+	return int16(wrapInt64Result(internalStr2Int(val, 16, true)))
+}
+
+// Convert string to int16, return the default value you set when the conversion fails
+func Str2Int16WithDefaultValue(val string, defaultVal int16) int16 {
+	if res, err := internalStr2Int(val, 16, false); err != nil {
+		return defaultVal
+	} else {
+		return int16(res)
+	}
 }
 
 // Convert string to int8, panic when the string is not of a valid int8 format.
 func Str2Int8(val string) int8 {
-	return int8(wrapResult(internalStr2Int(val, 8, false)))
+	return int8(wrapInt64Result(internalStr2Int(val, 8, false)))
 }
 
 // Convert string to int8, return 0 if the conversion fails
 func Str2Int8NoPanic(val string) int8 {
-	return int8(wrapResult(internalStr2Int(val, 8, true)))
+	return int8(wrapInt64Result(internalStr2Int(val, 8, true)))
+}
+
+// Convert string to int8, return the default value you set when the conversion fails
+func Str2Int8WithDefaultValue(val string, defaultVal int8) int8 {
+	if res, err := internalStr2Int(val, 8, false); err != nil {
+		return defaultVal
+	} else {
+		return int8(res)
+	}
 }
 
 // Convert string to int, panic when the string is not of a valid int format.
 func Str2Int(val string) int {
-	return int(wrapResult(internalStr2Int(val, 0, false)))
+	return int(wrapInt64Result(internalStr2Int(val, 0, false)))
 }
 
 // Convert string to int, return 0 if the conversion fails
 func Str2IntNoPanic(val string) int {
-	return int(wrapResult(internalStr2Int(val, 0, true)))
+	return int(wrapInt64Result(internalStr2Int(val, 0, true)))
 }
 
-func internalStr2Uint(val string, bitSize int, noPanic bool) uint64 {
+// Convert string to int, return the default value you set when the conversion fails
+func Str2IntWithDefaultValue(val string, defaultVal int) int {
+	if res, err := internalStr2Int(val, 0, false); err != nil {
+		return defaultVal
+	} else {
+		return int(res)
+	}
+}
+
+func internalStr2Uint(val string, bitSize int, noPanic bool) (uint64, error) {
 	res, err := strconv.ParseUint(val, 0, bitSize)
 	if err != nil{
 		if noPanic {
-			return 0
+			return 0, err
 		}else {
 			panic(err)
 		}
 	}
+	return res, nil
+}
+
+// Simply return the conversion result.
+// For conversions that may panic, the code won't come here.
+// For conversions that won't panic, res will be zero value and err will come here as well.
+func wrapUint64Result(res uint64, err error) uint64 {
 	return res
 }
 
 // Convert string to uint64, panic when the string is not of a valid uint64 format.
 func Str2Uint64(val string) uint64 {
-	return uint64(internalStr2Uint(val, 64, false))
+	return uint64(wrapUint64Result(internalStr2Uint(val, 64, false)))
 }
 
 // Convert string to uint64, return 0 if the conversion fails
 func Str2Uint64NoPanic(val string) uint64 {
-	return uint64(internalStr2Uint(val, 64, true))
+	return uint64(wrapUint64Result(internalStr2Uint(val, 64, true)))
+}
+
+// Convert string to uint64, return the default value you set when the conversion fails
+func Str2Uint64WithDefaultValue(val string, defaultVal uint64) uint64 {
+	if res, err := internalStr2Uint(val, 64, false); err != nil {
+		return defaultVal
+	} else {
+		return uint64(res)
+	}
 }
 
 // Convert string to uint32, panic when the string is not of a valid uint32 format.
 func Str2Uint32(val string) uint32 {
-	return uint32(internalStr2Uint(val, 32, false))
+	return uint32(wrapUint64Result(internalStr2Uint(val, 32, false)))
 }
 
 // Convert string to uint32, return 0 if the conversion fails
 func Str2Uint32NoPanic(val string) uint32 {
-	return uint32(internalStr2Uint(val, 32, true))
+	return uint32(wrapUint64Result(internalStr2Uint(val, 32, true)))
+}
+
+// Convert string to uint32, return the default value you set when the conversion fails
+func Str2Uint32WithDefaultValue(val string, defaultVal uint32) uint32 {
+	if res, err := internalStr2Uint(val, 32, false); err != nil {
+		return defaultVal
+	} else {
+		return uint32(res)
+	}
 }
 
 // Convert string to uint16, panic when the string is not of a valid uint16 format.
 func Str2Uint16(val string) uint16 {
-	return uint16(internalStr2Uint(val, 16, false))
+	return uint16(wrapUint64Result(internalStr2Uint(val, 16, false)))
 }
 
 // Convert string to uint16, return 0 if the conversion fails
 func Str2Uint16NoPanic(val string) uint16 {
-	return uint16(internalStr2Uint(val, 16, true))
+	return uint16(wrapUint64Result(internalStr2Uint(val, 16, true)))
+}
+
+// Convert string to uint16, return the default value you set when the conversion fails
+func Str2Uint16WithDefaultValue(val string, defaultVal uint16) uint16 {
+	if res, err := internalStr2Uint(val, 16, false); err != nil {
+		return defaultVal
+	} else {
+		return uint16(res)
+	}
 }
 
 // Convert string to uint8, panic when the string is not of a valid uint8 format.
 func Str2Uint8(val string) uint8 {
-	return uint8(internalStr2Uint(val, 8, false))
+	return uint8(wrapUint64Result(internalStr2Uint(val, 8, false)))
 }
 
 // Convert string to uint8, return 0 if the conversion fails
 func Str2Uint8NoPanic(val string) uint8 {
-	return uint8(internalStr2Uint(val, 8, true))
+	return uint8(wrapUint64Result(internalStr2Uint(val, 8, true)))
+}
+
+// Convert string to uint16, return the default value you set when the conversion fails
+func Str2Uint8WithDefaultValue(val string, defaultVal uint8) uint8 {
+	if res, err := internalStr2Uint(val, 8, false); err != nil {
+		return defaultVal
+	} else {
+		return uint8(res)
+	}
 }
 
 // Convert string to uint, panic when the string is not of a valid uint format.
 func Str2Uint(val string) uint {
-	return uint(internalStr2Uint(val, 0, false))
+	return uint(wrapUint64Result(internalStr2Uint(val, 0, false)))
 }
 
 // Convert string to uint, return 0 if the conversion fails
 func Str2UintNoPanic(val string) uint {
-	return uint(internalStr2Uint(val, 0, true))
+	return uint(wrapUint64Result(internalStr2Uint(val, 0, true)))
 }
 
-func internalStr2Float(val string, bitSize int, noPanic bool) float64 {
+// Convert string to uint, return the default value you set when the conversion fails
+func Str2UintWithDefaultValue(val string, defaultVal uint) uint {
+	if res, err := internalStr2Uint(val, 0, false); err != nil {
+		return defaultVal
+	} else {
+		return uint(res)
+	}
+}
+
+func internalStr2Float(val string, bitSize int, noPanic bool) (float64, error) {
 	res, err := strconv.ParseFloat(val, bitSize)
 	if err != nil{
 		if noPanic {
-			return 0
+			return 0, err
 		}else {
 			panic(err)
 		}
 	}
+	return res, nil
+}
+
+// Simply return the conversion result.
+// For conversions that may panic, the code won't come here.
+// For conversions that won't panic, res will be zero value and err will come here as well.
+func wrapFloat64Result(res float64, err error) float64 {
 	return res
 }
 
 // Convert string to float64, panic when the string is not of a valid float64 format.
 func Str2Float64(val string) float64 {
-	return float64(internalStr2Float(val, 64, false))
+	return float64(wrapFloat64Result(internalStr2Float(val, 64, false)))
+}
+
+// Convert string to float64, return 0 if the conversion fails
+func Str2Float64NoPanic(val string) float64 {
+	return float64(wrapFloat64Result(internalStr2Float(val, 64, true)))
+}
+
+// Convert string to float64, return the default value you set when the conversion fails
+func Str2Float64WithDefaultValue(val string, defaultVal float64) float64 {
+	if res, err := internalStr2Float(val, 64, false); err != nil {
+		return defaultVal
+	} else {
+		return float64(res)
+	}
 }
 
 // Convert string to float32, panic when the string is not of a valid float32 format.
 func Str2Float32(val string) float32 {
-	return float32(internalStr2Float(val, 32, false))
+	return float32(wrapFloat64Result(internalStr2Float(val, 32, false)))
+}
+
+// Convert string to float32, return 0 if the conversion fails
+func Str2Float32NoPanic(val string) float32 {
+	return float32(wrapFloat64Result(internalStr2Float(val, 32, true)))
+}
+
+// Convert string to float32, return the default value you set when the conversion fails
+func Str2Float32WithDefaultValue(val string, defaultVal float32) float32 {
+	if res, err := internalStr2Float(val, 32, false); err != nil {
+		return defaultVal
+	} else {
+		return float32(res)
+	}
 }
 
 // Convert string to bool, panic when the string is not of a valid bool format
